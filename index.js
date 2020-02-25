b6b6926ed05241d34d2a54d05a26f904fa786be3
Aliadabi <Aliadabi3780@gmail.com>
2020-02-25 15:14:41 -0800

    needs alot of work

--- index.js
+++ index.js
@@ -39,10 +39,14 @@ function addNumbers(num1, num2) {
  *   email: "leia@leia.com",
  * }
 */
-function makePersonObject(/* code here */) {
-  /* code here */
-  
+function makePersonObject(id, name, email) {
+  let ourobj = {
+    Id : `${id}`,
+    Name : `${name}`,
+    Email : `${email}`
+  }
 }
+console.log(makePersonObject('2','Ali','ALiAli@Ali.Ali'))
 
 /**
  * ### Challenge `getName`
@@ -57,8 +61,8 @@ function makePersonObject(/* code here */) {
  * passing { id: 1, name: 'Leia', email: 'leia@leia.com` } as the argument,
  * the returned value should look like `Hello, my name is Leia`.
 */
-function getName(/* code here */) {
-  /* code here */
+function getName(datagiven) {
+  return `hello, my name is ${obj.name}`
 }
 
 /**
@@ -74,8 +78,18 @@ function getName(/* code here */) {
  *         and returns a string like `Hello, my name is {name}`.
  *         where `{name}` is the name passed into `makeSmartPerson`.
 */
-function makeSmartPerson(/* code here */) {
-  /* code here */
+function makeSmartPerson(name) {
+  let obj = {
+    Name : `${name}`,
+    Sum : function(num1, num2){
+      return num1 + num2
+    },
+    speak: function(){
+        return `Hello, my name is ${name}`.
+        
+    }
+  }
+  
 }
 
 
